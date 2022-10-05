6de8cd599860122ccafa8096503cd25e2621febb
Kiyun <104542035+dlrldbs@users.noreply.github.com>
2022-10-05 18:03:45 +0900

    Day 14

---
+++ java-3/src/xyz/itwill/thread/SingleThread.java
@@ -0,0 +1,8 @@
+package xyz.itwill.thread;
+
+public class SingleThread {
+	public void display() {
+		System.out.println("SingleThread 클래스의 display() 메소드 시작");
+		System.out.println("SingleThread 클래스의 display() 메소드 종료");
+	}
+}
